fca5c5159641073acc3362f78506a5154b5f28e4
Matt Stegall <mstegall928@gmail.com>
2017-09-05 17:59:57 -0400

    9/5

---
+++ 2017-09-05/exercise2.js
@@ -0,0 +1,33 @@
+var employees = [
+    {
+        name: "doug",
+        payRate: 40,
+        hourWorked: 4,
+        employee_type: "contractor"
+    },
+    {
+        name: "AJ",
+        payRate: 60,
+        hourWorked: 1,
+        employee_type: "full-time"
+    },
+    {
+        name: "jessica",
+        payRate: 41,
+        hourWorked: 7,
+        employee_type: "contractor"
+    },
+    {
+        name: "rachel",
+        payRate: 30,
+        hourWorked: 10,
+        employee_type: "full-time"
+    },
+    {
+        name: "andy",
+        payRate: 100,
+        hourWorked: 20,
+        employee_type: "contractor"
+    },
+]
+
